a80ea1bfdf23fe66ce5c81b4441442297fb3eac9
Ruben Zantingh <rubenpeterzantingh@gmail.com>
2024-06-16 09:11:29 +0200

    Initial commit

---
+++ MultiLanguage_DE.lua
@@ -0,0 +1,34 @@
+local addonName = ...
+local optionsFrame = CreateFrame("Frame")
+
+local function languageExists(value)
+    for _, language in ipairs(MultiLanguageOptions.AVAILABLE_LANGUAGES) do
+        if language.value == value then
+            return true
+        end
+    end
+    return false
+end
+
+local function addLanguageOption()
+    if not languageExists('de') then
+        table.insert(MultiLanguageOptions.AVAILABLE_LANGUAGES, {value = 'de', text = 'German'})
+        AddLanguageDropdownOption()
+    end
+
+    if MultiLanguageTranslations['de'] == nil then
+        MultiLanguageTranslations['de'] = {
+            description = 'Beschreibung',
+            objectives = 'Questziele'
+        }
+    end
+end
+
+local function addonLoaded(self, event, addonLoadedName)
+    if addonLoadedName == addonName then
+        addLanguageOption()
+    end
+end
+
+optionsFrame:RegisterEvent("ADDON_LOADED")
+optionsFrame:SetScript("OnEvent", addonLoaded)
